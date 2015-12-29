8b15a961a5847f292f9e21f454f0860a5081a3fa
Rico75 <rpeinado75@gmail.com>
2015-12-29 00:10:03 -0800

    Update css and form functionality

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -6,8 +6,6 @@ import styles from './ContactPage.css';
 import withStyles from '../../decorators/withStyles';
 export const fields = ['firstName', 'lastName', 'email'];
 
-
-
 @withStyles(styles)
 class ContactPage extends Component {
 
@@ -22,10 +20,9 @@ class ContactPage extends Component {
     onSetTitle: PropTypes.func.isRequired
   };
 
-
   render() {
     const {
-      fields: {firstName, lastName, email},
+      fields: {firstName, lastName, email, notes},
       handleSubmit,
       resetForm,
       submitting
@@ -35,31 +32,51 @@ class ContactPage extends Component {
     this.context.onSetTitle(title);
 
     return (
-      <div className="ContactPage-form">
-        <h1>{title}</h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>First Name</label>
-            <input type="text" placeholder="First Name" {...firstName}/>
-          </div>
-          <div>
-            <label>Last Name</label>
-            <input type="text" placeholder="Last Name" {...lastName}/>
-          </div>
-          <div>
-            <label>Email</label>
-            <input type="email" placeholder="Email" {...email}/>
-          </div>
-          <button onClick={handleSubmit}>Submit</button>
-        </form>
+      <div className="ContactPage">
+        <div className="ContactPage-form">
+          <h1>{title}</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>First Name</label>
+              <input type="text" placeholder="First Name" {...firstName}/>
+            </div>
+            <div>
+              <label>Last Name</label>
+              <input type="text" placeholder="Last Name" {...lastName}/>
+            </div>
+            <div>
+              <label>Email</label>
+              <input type="email" placeholder="Email" {...email}/>
+            </div>
+            <div>
+              <label>Message</label>
+              <textarea
+                {...notes}
+                value={notes.value || ''}       // required for reset form to work (only on textarea's)
+                                                // see: https://github.com/facebook/react/issues/2533
+              />
+            </div>
+
+            <div>
+              <button disabled={submitting} onClick={handleSubmit} className="btn btn-primary btn-lg">
+                {submitting ? <i className="fa fa-paper-plane" /> : <i className="fa fa-paper-plane" />} Submit
+              </button>
+              <button disabled={submitting} onClick={resetForm}>
+                Clear Values
+              </button>
+            </div>
+
+          </form>
+        </div>
       </div>
+
     );
   }
 }
 
 ContactPage = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
   form: 'contact',                           // a unique name for this form
-  fields: ['firstName', 'lastName', 'email'] // all the fields in your form
+  fields: ['firstName', 'lastName', 'email', 'notes'] // all the fields in your form
 })(ContactPage);
 
 export default ContactPage;
